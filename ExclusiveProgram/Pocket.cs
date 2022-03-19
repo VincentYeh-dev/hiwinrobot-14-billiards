9fa4d9e4a3c4bec9f26856b7debf3a8a6e338d0b
ZiTe <honmonoh@gmail.com>
2022-03-19 14:48:18 +0800

    Add Packet class （#5）

---
+++ ExclusiveProgram/Pocket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExclusiveProgram
+{
+    /// <summary>
+    /// 球袋種類。
+    /// </summary>
+    public enum PocketType
+    {
+        /// <summary>
+        /// 邊。
+        /// </summary>
+        Side,
+
+        /// <summary>
+        /// 角落。
+        /// </summary>
+        Corner
+    }
+
+    /// <summary>
+    /// 球袋。
+    /// </summary>
+    public class Pocket
+    {
+        /// <summary>
+        /// 種類。
+        /// </summary>
+        public readonly PocketType Type;
+
+        /// <summary>
+        /// 位置。
+        /// </summary>
+        public readonly PointF Position;
+
+        /// <summary>
+        /// 球袋。
+        /// </summary>
+        /// <param name="type">種類。</param>
+        /// <param name="position">位置。</param>
+        public Pocket(PocketType type, PointF position)
+        {
+            Type = type;
+            Position = position;
+        }
+    }
+}
